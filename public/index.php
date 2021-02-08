83dc420094d09385fa50850897cd4d12ba871ebb
wcoxon <55706738+wcoxon@users.noreply.github.com>
2021-02-08 15:36:19 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
+
 <?php 
-echo "https://plymouth.thedata.place/dataset/manual-people-counters-in-central-park/resource/cda4367c-090f-42e7-80ae-827b19e3fc07"
-echo "https://developers.google.com/maps/documentation/urls/get-started"
-
+echo "https://plymouth.thedata.place/dataset/manual-people-counters-in-central-park/resource/cda4367c-090f-42e7-80ae-827b19e3fc07";
+echo "https://developers.google.com/maps/documentation/urls/get-started";
 ?>
-
